7359d0b34499b9cde0b6be6506a2c328680ea2fc
Ville Saukkonen <villesau@msn.com>
2022-12-31 18:06:40 +0200

    basic relay transform

--- transform.ts
+++ transform.ts
@@ -28,6 +28,13 @@ const transform: Transform = (file, api) => {
       const functionNames = ['mutationField', 'queryField'];
       return callee.type === 'Identifier' && functionNames.includes(callee.name) && p.value.arguments[0].type === 'StringLiteral';
     });
+  const relayQueries = root
+    .find(j.CallExpression).filter(p => {
+      const callee = p.value.callee;
+      const functionNames = ['mutationField', 'queryField'];
+      // matches arrow functions
+      return callee.type === 'Identifier' && functionNames.includes(callee.name) && p.value.arguments[0].type === 'ArrowFunctionExpression';
+    });
   enums.replaceWith(p => {
     const name = p.value.arguments[0].properties.find((p: any) => p.key.name === 'name').value;
     return statement`builder.enumType(${name}, {
@@ -52,13 +59,13 @@ const transform: Transform = (file, api) => {
         const name = p.key.name;
         const nonNullable = p.value.callee.name === 'nonNull';
         const params = []
-        if(nonNullable) {
-          params.push( j.property('init', j.identifier('required'), j.booleanLiteral(true)))
+        if (nonNullable) {
+          params.push(j.property('init', j.identifier('required'), j.booleanLiteral(true)))
         }
         const val = p.value.arguments[0] ?? p.value;
         let newArg;
-        if(val.type === 'Identifier') {
-          params.unshift( j.property('init', j.identifier('type'), val))
+        if (val.type === 'Identifier') {
+          params.unshift(j.property('init', j.identifier('type'), val))
           newArg = j.callExpression(
             j.memberExpression(j.identifier('t'), j.identifier('arg')),
             [j.objectExpression(params)]
@@ -75,22 +82,92 @@ const transform: Transform = (file, api) => {
     }
     const typeProperty = config.properties.find(p => p.key.name === 'type')
     const isNullable = typeProperty.value.type === 'CallExpression' && typeProperty.value.callee.name === 'nullable'
-    if(isNullable) {
-      if(typeProperty.value.arguments[0]?.callee?.name === 'list') {
+    if (isNullable) {
+      if (typeProperty.value.arguments[0]?.callee?.name === 'list') {
         typeProperty.value = j.arrayExpression([typeProperty.value.arguments[0].arguments[0]]);
       } else {
         typeProperty.value = typeProperty.value.arguments[0];
       }
-    } else if(typeProperty.value.type === 'CallExpression' && typeProperty.value.callee.name === 'list') {
+    } else if (typeProperty.value.type === 'CallExpression' && typeProperty.value.callee.name === 'list') {
       typeProperty.value = j.arrayExpression([typeProperty.value.arguments[0]]);
     }
     return statement`builder.${functionName}(${j.stringLiteral(name)}, t => t.field(${j.objectExpression([
       typeProperty,
-      isNullable ? j.property('init', j.identifier('nullable'), j.booleanLiteral(true)): null,
+      isNullable ? j.property('init', j.identifier('nullable'), j.booleanLiteral(true)) : null,
       config.properties.find(p => p.key.name === 'args'),
       auth,
       config.properties.find(p => p.key.name === 'resolve')].filter(Boolean))}))`;
-  })
+  });
+
+  relayQueries.replaceWith(p => {
+    const functionName = p.value.callee.name;
+    const args = p.value.arguments;
+    if (args.length === 0) {
+      return p.value;
+    }
+    const connectionFieldArguments = args[0].body.body[0].expression.arguments;
+    const name = connectionFieldArguments[0].value;
+    const config = connectionFieldArguments[1];
+    const typeProperty = config.properties.find(p => p.key.name === 'type');
+    const isNullable = typeProperty.value.type === 'CallExpression' && typeProperty.value.callee.name === 'nullable';
+    if (isNullable) {
+      typeProperty.value = typeProperty.value.arguments[0];
+    }
+    const additionalArgs = config.properties.find(p => p.key.name === 'additionalArgs');
+    if (additionalArgs) {
+      additionalArgs.key.name = 'args';
+    }
+    const nodes = config.properties.find(p => p.key.name === 'nodes');
+    if (nodes) {
+      nodes.key.name = 'resolve';
+      const previousBody = nodes.body;
+      const isAsync = nodes.async;
+      nodes.async = false;
+      nodes.body = j.blockStatement([
+        j.returnStatement(
+          j.callExpression(
+            j.identifier('resolveOffsetConnection'),
+            [
+              j.objectExpression([
+                j.property.from({
+                  kind: 'init',
+                  key: j.identifier('args'),
+                  value: j.identifier('args'),
+                  shorthand: true
+                })
+              ]),
+              j.arrowFunctionExpression.from({
+                  params: [
+                    j.objectPattern([
+                      j.property.from({
+                        kind: 'init',
+                        key: j.identifier('limit'),
+                        value: j.identifier('limit'),
+                        shorthand: true
+                      }),
+                      j.property.from({
+                        kind: 'init',
+                        key: j.identifier('offset'),
+                        value: j.identifier('offset'),
+                        shorthand: true
+                      })
+                    ])
+                  ],
+                  body: previousBody,
+                  async: isAsync
+                }
+              )
+            ]
+          )
+        )
+      ]);
+    }
+    return statement`builder.${functionName}(${j.stringLiteral(name)}, t => t.connection(${j.objectExpression([
+      typeProperty,
+      isNullable ? j.property('init', j.identifier('nullable'), j.booleanLiteral(true)) : null,
+      additionalArgs,
+      nodes].filter(Boolean))}))`;
+  });
 
   objects.replaceWith(p => {
     const object = p.value.arguments[0];
@@ -102,18 +179,18 @@ const transform: Transform = (file, api) => {
       let propertyName;
       let type;
       let resolve;
-      if(functionName === 'field' && node.expression.arguments[0].type === 'ObjectExpression') {
-         propertyName = node.expression.arguments[0].properties.find(p => p.key.name === 'name').value;
-         type = node.expression.arguments[0].properties.find(p => p.key.name === 'type').value;
-         resolve = node.expression.arguments[0].properties.find(p => p.key.name === 'resolve');
-      } else if(functionName === 'field') {
-         propertyName = node.expression.arguments[0];
-         type = node.expression.arguments[1].properties.find(p => p.key.name === 'type').value;
-         resolve = node.expression.arguments[1].properties.find(p => p.key.name === 'resolve')
+      if (functionName === 'field' && node.expression.arguments[0].type === 'ObjectExpression') {
+        propertyName = node.expression.arguments[0].properties.find(p => p.key.name === 'name').value;
+        type = node.expression.arguments[0].properties.find(p => p.key.name === 'type').value;
+        resolve = node.expression.arguments[0].properties.find(p => p.key.name === 'resolve');
+      } else if (functionName === 'field') {
+        propertyName = node.expression.arguments[0];
+        type = node.expression.arguments[1].properties.find(p => p.key.name === 'type').value;
+        resolve = node.expression.arguments[1].properties.find(p => p.key.name === 'resolve')
       } else {
-         propertyName = node.expression.arguments[0];
-         type = functionName;
-         resolve = node.expression.arguments[1]?.properties.find(p => p.key.name === 'resolve')
+        propertyName = node.expression.arguments[0];
+        type = functionName;
+        resolve = node.expression.arguments[1]?.properties.find(p => p.key.name === 'resolve')
       }
       const exposeName = functionName === 'field' ? 'expose' : `expose${capitalizeFirstLetter(type === 'id' ? 'ID' : type)}`;
       const objectProps = [];
